b8cff2ffe5dda3af327be8ffce7ce38348a8499e
vijay18399 <vijayreddy18399@gmail.com>
2018-12-22 19:48:50 +0530

    technews completed

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@ var monk = require('monk');
 
 var db = monk('localhost:27017/clgnews');
 var trainings=db.get('trainings');
-var trainers=db.get('trainers');
-var circular=db.get('circular');
+var workshops=db.get('workshops');
+var seminars=db.get('seminars');
+var eventgallery=db.get('eventgallery');
+var placedcandidates=db.get('placedcandidates');
+var cdrive=db.get('cdrive');
+var exam=db.get('exam');
+var techs=db.get('techbit');
 var articles=db.get('articles');
+var trainers=db.get('trainers');
+var slider=db.get('slider');
+var notice=db.get('notice');
+var circular=db.get('circular');
 router.get('/user', function(req, res) { 
 	 
 	     
@@ -19,24 +28,30 @@ router.get('/adminpanel', function(req, res) {
  });
 router.get('/', function(req, res) { 
 		trainings.find({},{}, function(e, trainings){ 
-                  
-              console.log(trainings);
-            articles.find({},{}, function(e, docs){  
-console.log(docs);
-
+            articles.find({},{}, function(e, articles){ 
+             techs.find({},{}, function(e, techs){  
+           cdrive.find({},{}, function(e, cdrive){
+            circular.find({},{}, function(e, circular){
+                exam.find({},{}, function(e, exam){
+           seminars.find({},{}, function(e, seminars){
+                notice.find({},{}, function(e, notice){
        res.render('index', {   
 
-                 'articles': docs, 
-            'trainings': trainings
-
+                 'articles': articles, 
+            
+            'trainings': trainings,
+            'cdrive':cdrive,
+            'circular':circular,
+             'exam':exam,
+             
+             'techs':techs,
+             'notice':notice
 
            }); 
-		   });  
-	  
-           }); 
-          
-          
-     }); 
+
+
+        }); }); }); }); }); }); }); 
+ });  });
            
  
 
